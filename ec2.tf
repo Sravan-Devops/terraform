d0ed184bb93ace3f38719a1853ed848d39df9a6d
Sravan Kumar <yadav.sravan1990@gmail.com>
2025-05-02 14:31:23 +0530

    add

---
+++ ec2.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "web" {
+  ami           = "ami-01fc98868662094f9"
+  instance_type = "t2.micro"
+
+
+  tags = {
+    Name = "DevOps-EC2"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
